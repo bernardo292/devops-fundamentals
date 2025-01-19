acd04b02c6a819fef52a2940e83ea71987b7bb0e
bernardo292 <ebaieyonganyibernardo2020@gmail.com>
2025-01-19 14:46:21 +0100

    updated code

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "vpc_module" {
-  source       = "./module/vpc"
+  source       = "./terraform_ec2/module/vpc"
   aws_vpc_name = var.aws_vpc_name
 }
 
 
 module "ec2_module" {
-  source                = "./module/ec2"
+  source                = "./terraform_ec2/module/ec2"
 #   instance_name         = var.instance_name
   role_name             = var.role_name
   policy_name           = var.policy_name
